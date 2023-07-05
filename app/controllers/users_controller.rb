9ad469d6fec5c0c99a6dc0ccb2a41d60686961cd
Yannick.Mathieu <yannick.mathieu@toolib.fr>
2023-07-05 11:07:09 +0200

    maj postgresql connection

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        # Opérations à effectuer en cas de réussite (par exemple, redirection, réponse JSON, etc.)
+        puts "Utilisateur enregistré avec succès !"
+      else
+        # Opérations à effectuer en cas d'échec (par exemple, affichage des erreurs, réponse d'erreur, etc.)
+        puts "Échec de l'enregistrement de l'utilisateur :"
+        puts @user.errors.full_messages
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :username) # Spécifiez les paramètres requis pour l'utilisateur
+    end
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
-end
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :username, presence: true, uniqueness: true
+  end
+  
